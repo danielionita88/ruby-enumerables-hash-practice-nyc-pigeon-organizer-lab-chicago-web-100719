bd0827b7b35411915c6a892010ba343293c5c418
Daniel I <danielionita88@yahoo.com>
2019-09-21 19:53:30 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -7,11 +7,11 @@ def nyc_pigeon_organizer(data)
   data.each do |atribute,value|
     value.each do |sub_atribute,pigeon_name|
       pigeon_name.each do |name|
-        if 
+        if !organized_data.has_key?(name)
         organized_data[name] = {} 
        
     
-    binding.pry
+    
       end
     end
   end
