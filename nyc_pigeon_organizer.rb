3e334edf6afff8380113e72e8ef7da1deef4131f
Daniel I <danielionita88@yahoo.com>
2019-09-21 20:03:42 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -9,7 +9,7 @@ def nyc_pigeon_organizer(data)
       pigeon_name.each do |name|
         organized_data[name] = {} if !organized_data[name]
         organized_data[name][atribute] =[] if !organized_data[name][atribute]
-        organized_data[name][atribute].push(sub_atribute.to_s)
+        organized_data[name][atribute].push(sub_atribute)
       end
     end
   end
